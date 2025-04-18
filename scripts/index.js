318a77a53ce0d03667787f065d2634473855c020
Linvelive <alina10.1179@gmail.com>
2025-04-17 21:30:17 -0400

    deleting cards

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,37 @@
 
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
 // @todo: Вывести карточки на страницу
+
+const cardList = document.querySelector(".places__list");
+
+// функция создания карточки
+function createCard(data, onDelete) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+
+  deleteButton.addEventListener("click", () => {
+    deleteCallback(cardElement);
+  });
+
+  return cardElement;
+}
+
+// функция удаления
+function handleDelete(cardElement) {
+  cardElement.remove();
+}
+
+// добавление карточек из initialCards
+initialCards.forEach((cardData) => {
+  const card = createCard(cardData, handleDelete);
+  cardList.appendChild(card);
+});
