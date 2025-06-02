e1841d96b6e97bc4dce1eb8d7c8de03293dad66a
Linvelive <alina10.1179@gmail.com>
2025-06-02 00:54:19 -0400

    webpack works

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,13 @@
 // @todo: DOM узлы
 
 // @todo: Вывести карточки на страницу
+import '../styles/index.css'; // добавьте импорт главного файла стилей
+import { initialCards } from './cards';
+import avatar from '../images/avatar.jpg';
+import logo from '../images/logo.svg';
+
+document.querySelector('.profile__image').style.backgroundImage = `url(${avatar})`;
+document.querySelector('.header__logo').src = logo;
 
 const cardList = document.querySelector(".places__list");
 
@@ -14,6 +21,8 @@ function createCard(data, onDelete) {
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__title");
   const deleteButton = cardElement.querySelector(".card__delete-button");
+
+ console.log(cardImage)
 
   cardImage.src = data.link;
   cardImage.alt = data.name;
@@ -36,3 +45,6 @@ initialCards.forEach((cardData) => {
   const card = createCard(cardData, handleDelete);
   cardList.appendChild(card);
 });
+
+
+
