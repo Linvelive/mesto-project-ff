d314ff60066f36bf75a76dac968f561745a97c8d
Linvelive <alina10.1179@gmail.com>
2025-06-03 03:07:23 -0400

    popup image, delete card

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,15 +3,24 @@
 // @todo: DOM узлы
 
 // @todo: Вывести карточки на страницу
-import '../styles/index.css'; // добавьте импорт главного файла стилей
-import { initialCards } from './cards';
-import avatar from '../images/avatar.jpg';
-import logo from '../images/logo.svg';
+import "../styles/index.css"; // добавьте импорт главного файла стилей
+import { initialCards } from "./cards";
+import avatar from "../images/avatar.jpg";
+import logo from "../images/logo.svg";
 
-document.querySelector('.profile__image').style.backgroundImage = `url(${avatar})`;
-document.querySelector('.header__logo').src = logo;
+document.querySelector(
+  ".profile__image"
+).style.backgroundImage = `url(${avatar})`;
+document.querySelector(".header__logo").src = logo;
 
 const cardList = document.querySelector(".places__list");
+
+//модалки
+const editModal = document.querySelector(".popup_type_edit");
+const addModal = document.querySelector(".popup_type_new-card");
+const imageModal = document.querySelector(".popup_type_image");
+const popUpImage = imageModal.querySelector(".popup__image");
+const popUpCaption = imageModal.querySelector(".popup__caption");
 
 // функция создания карточки
 function createCard(data, onDelete) {
@@ -22,14 +31,19 @@ function createCard(data, onDelete) {
   const cardTitle = cardElement.querySelector(".card__title");
   const deleteButton = cardElement.querySelector(".card__delete-button");
 
- console.log(cardImage)
+  console.log(cardImage);
 
   cardImage.src = data.link;
   cardImage.alt = data.name;
   cardTitle.textContent = data.name;
-
   deleteButton.addEventListener("click", () => {
-    deleteCallback(cardElement);
+    onDelete(cardElement);
+  });
+
+  cardImage.addEventListener("click", () => {
+    imageModal.classList.add("popup_is-opened");
+    popUpImage.src = data.link;
+    popUpCaption.textContent = data.name;
   });
 
   return cardElement;
@@ -46,5 +60,12 @@ initialCards.forEach((cardData) => {
   cardList.appendChild(card);
 });
 
+//элементы для попапа
+const editButton = document.querySelector(".profile__edit-button");
+const addButton = document.querySelector(".profile__add-button");
+const image = document.querySelector(".card__image");
 
+//закрытие крестиком
+const closeButton = document.querySelector(".popup__close");
 
+//функции
